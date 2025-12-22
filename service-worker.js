c77f4729a92d30c75df6156eded3e92690481006
burni2001 <bernhard@mueller.oettlin.com>
2025-12-22 22:33:07 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mission-debrief-v4.5 Alpha4"; // Increment this version number when you make changes to cached assets
+const CACHE_NAME = "mission-debrief-v4.5 Alpha5"; // Increment this version number when you make changes to cached assets
 const urlsToCache = [
     "./", // Caches the root, i.e., index.html
     "./index.html",
